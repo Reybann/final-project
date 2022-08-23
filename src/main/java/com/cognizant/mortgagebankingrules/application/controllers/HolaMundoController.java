fbe872885f920c95a6be6526bec8a1dbf8fe58b8
Reybann <mahat2mit@gmail.com>
2022-08-23 11:03:09 +0200

    first commit holaMundo endpoint

---
+++ src/main/java/com/cognizant/mortgagebankingrules/application/controllers/HolaMundoController.java
@@ -0,0 +1,13 @@
+package com.cognizant.mortgagebankingrules.application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return String.format("Hello moto!");
+    }
+}
