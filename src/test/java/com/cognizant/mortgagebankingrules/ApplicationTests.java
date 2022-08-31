b5e56ac4168afe81c94c9d28b34d707afab795a7
Raúl A. Puigbó Méndez <raulapuigbo@gmail.com>
2022-08-31 21:49:12 +0200

    Added test for create rule

--- src/test/java/com/cognizant/mortgagebankingrules/ApplicationTests.java
+++ src/test/java/com/cognizant/mortgagebankingrules/ApplicationTests.java
@@ -1,14 +1,21 @@
 package com.cognizant.mortgagebankingrules;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.cognizant.mortgagebankingrules.domain.services.RuleClassService;
+import com.cognizant.mortgagebankingrules.domain.services.RuleDomainService;
+import com.cognizant.mortgagebankingrules.domain.services.RuleService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-		
-	}
 
 }
