c6665182813a0b2c625bf294e094a15a16157fb9
Raúl A. Puigbó Méndez <raulapuigbo@gmail.com>
2022-09-01 13:59:22 +0200

    Testing saving method of repository

--- src/test/java/com/cognizant/mortgagebankingrules/RepositoryTest.java
+++ src/test/java/com/cognizant/mortgagebankingrules/RepositoryTest.java
@@ -1,15 +1,40 @@
 package com.cognizant.mortgagebankingrules;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.cognizant.mortgagebankingrules.domain.Rule;
-import com.cognizant.mortgagebankingrules.insfrastructure.repository.RuleRepository;
+import com.cognizant.mortgagebankingrules.domain.repositories.RuleClassRepository;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
 public class RepositoryTest {
+
    @Autowired
-    private RuleRepository ruleRepository;
+    private RuleClassRepository ruleRepository ;
     
     private Rule rule;
 
+    @BeforeEach
+    public void setUp() {
+        rule =  Rule.builder().id(UUID.randomUUID()).name("rule1").enabled(true).duration(3).build();
+    }
+
+    @Test
+    public void givenRule_whenSave_thenReturnRule() {
+        
+        Rule rule = Rule.builder().id(UUID.randomUUID()).name("rule1").enabled(true).duration(3).build();
+
+        Rule savedRule = ruleRepository.save(rule);
+
+        assertThat(savedRule.getId()).isNotNull();
+        assertThat(savedRule.getName()).isEqualTo("rule1");
+        assertThat(savedRule.isEnabled()).isTrue();
+        assertThat(savedRule.getDuration()).isEqualTo(3);
+    }
+    
 }
