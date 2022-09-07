5907e7cd1481bc579b41e059d5fcdc3fc1f8926a
Mikel Fuentes <mikel.aingeru.fuentes@gmail.com>
2022-09-07 16:59:34 +0200

    Controller update rule test

--- src/test/java/com/cognizant/mortgagebankingrules/ControllerTest.java
+++ src/test/java/com/cognizant/mortgagebankingrules/ControllerTest.java
@@ -11,6 +11,7 @@ import com.cognizant.mortgagebankingrules.application.controllers.RulesControlle
 import com.cognizant.mortgagebankingrules.domain.RuleClass;
 import com.cognizant.mortgagebankingrules.domain.services.RuleClassService;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.UUID;
@@ -57,5 +58,39 @@ public class ControllerTest {
         mockMvc.perform(post("/rules/createrules").contentType("application/json").content(
                 "{\"id\":\"" + rule.getId() + "\",\"name\":\"" + rule.getName() + "\",\"enabled\":\"" + rule.isEnabled()
                         + "\",\"duration\":\"" + rule.getDuration() + "\"}")).andExpect(status().isNotFound());
+    }
+    @Test
+    public void testUpdateRule() throws Exception {
+
+        RuleClass rule = RuleClass.builder().id(UUID.randomUUID()).name("rule2").enabled(true).duration(6).build();
+
+        Mockito.when(ruleService.updateRuleClass(rule)).thenReturn(rule);
+
+        mockMvc.perform(put("/rules/updaterule").contentType("application/json").content(
+                "{\"id\":\"" + rule.getId() + "\",\"name\":\"" + "rul3" + "\",\"enabled\":\"" + false
+                        + "\",\"duration\":\"" + 2 + "\"}")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateRule_fail_badRequest() throws Exception {
+
+        RuleClass rule = RuleClass.builder().id(UUID.randomUUID()).name("rule3").enabled(false).duration(65).build();
+
+        Mockito.when(ruleService.updateRuleClass(rule)).thenReturn(rule);
+
+        mockMvc.perform(post("/rules/createrule").contentType("application/json").content(
+                "{\"id\":\"" + rule.getId() + "\",\"name\":\"" + "rule.getName()" + "\",\"enabled\":\"" + "rule.isEnabled()"
+                        + "\",\"duration\":\"" + "rule.getDuration()" + "\"}")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUpdateRule_fail_notFound() throws Exception {
+
+        RuleClass rule = RuleClass.builder().id(UUID.randomUUID()).name("rule4").enabled(true).duration(2).build();
+
+        Mockito.when(ruleService.updateRuleClass(rule)).thenReturn(rule);
+        mockMvc.perform(post("/rules/update").contentType("application/json").content(
+                "{\"id\":\"" + rule.getId() + "\",\"name\":\"" + "ruel" + "\",\"enabled\":\"" + false
+                        + "\",\"duration\":\"" + 5 + "\"}")).andExpect(status().isNotFound());
     }
 }
